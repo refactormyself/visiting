8d94bd253caff6a08b2bde11a9424ff57652fd00
SAHEED <refactormyself@gmail.com>
2021-05-07 15:00:00 +0100

    Update and reformat

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
-#include <iostream>
+#include "visitor.hpp"
 
+using namespace visit;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    // declare operations to be performed
+    Talk talk;
+    Walk walk;
+
+    Cat cat;
+    cat.perform(talk);
+    cat.perform(walk);
+
+    Dog dog;
+    dog.perform(talk);
+    dog.perform(walk);
+
+    Bird bird;
+    bird.perform(talk);
+    bird.perform(walk);
+
+    Penguin penguin;
+    penguin.perform(talk);
+    penguin.perform(walk);
+
+    return 0;    return 0;
 }
